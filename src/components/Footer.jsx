c91e8eacbe694f2f652d3b6afe367e3755c0f4d2
Luca Del Corona <luca.delcorona@techedgegroup.com>
2022-11-15 23:24:02 +0100

    Added footer + fix contact us

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className="py-5 text-center">
+            <p className="text-sm mt-2 opacity-50">
+                &copy; {new Date().getFullYear()} Luca Del Corona. All rights reserved.
+            </p>
         </div>
     );
 };
