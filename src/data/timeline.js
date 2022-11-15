92a29a1032649deb45f65f541cf42f8d51eb81a2
VoidElle <lucadelc@gmail.com>
2022-11-15 22:59:52 +0100

    Added Timeline

---
+++ src/data/timeline.js
@@ -0,0 +1,50 @@
+export default [
+    {
+        year: 'Today',
+        title: 'Full Stack developer',
+        duration: '6 years',
+        details: 'Design, development and maintenance of hybrid applications for mobile devices and microservices.\n' +
+            'Internally, I performed the following tasks:\n' +
+            '- Design of UI/UX interfaces for mobile applications\n' +
+            '- Develop cross-platform hybrid applications with Flutter and Dart\n' +
+            '- Development of RESTful APIs\n' +
+            '- Design and maintenance of NoSQL databases such as MongoDB\n' +
+            '- Implementation of ads within mobile applications'
+    },
+    {
+        year: 'Today',
+        title: 'Full Stack developer',
+        duration: '6 years',
+        details: 'Design, development and maintenance of hybrid applications for mobile devices and microservices.\n' +
+            'Internally, I performed the following tasks:\n' +
+            '- Design of UI/UX interfaces for mobile applications\n' +
+            '- Develop cross-platform hybrid applications with Flutter and Dart\n' +
+            '- Development of RESTful APIs\n' +
+            '- Design and maintenance of NoSQL databases such as MongoDB\n' +
+            '- Implementation of ads within mobile applications'
+    },
+    {
+        year: 'Today',
+        title: 'Full Stack developer',
+        duration: '6 years',
+        details: 'Design, development and maintenance of hybrid applications for mobile devices and microservices.\n' +
+            'Internally, I performed the following tasks:\n' +
+            '- Design of UI/UX interfaces for mobile applications\n' +
+            '- Develop cross-platform hybrid applications with Flutter and Dart\n' +
+            '- Development of RESTful APIs\n' +
+            '- Design and maintenance of NoSQL databases such as MongoDB\n' +
+            '- Implementation of ads within mobile applications'
+    },
+    {
+        year: 'Today',
+        title: 'Full Stack developer',
+        duration: '6 years',
+        details: 'Design, development and maintenance of hybrid applications for mobile devices and microservices.\n' +
+            'Internally, I performed the following tasks:\n' +
+            '- Design of UI/UX interfaces for mobile applications\n' +
+            '- Develop cross-platform hybrid applications with Flutter and Dart\n' +
+            '- Development of RESTful APIs\n' +
+            '- Design and maintenance of NoSQL databases such as MongoDB\n' +
+            '- Implementation of ads within mobile applications'
+    }
+]
