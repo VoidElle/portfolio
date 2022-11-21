864fd7b62386d31c86496fbb30930e6342174c34
VoidElle <lucadelc@gmail.com>
2022-11-22 00:59:01 +0100

    Upload new data + Styling fix

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,23 +1,11 @@
+import portfolioImage from "../assets/portfolio.png";
+
 export default [
     {
         id: 0,
-        title: "Test",
-        imgUrl: "https://via.placeholder.com/150",
+        title: "Portfolio",
+        imgUrl: portfolioImage,
         stack: ["HTML", "CSS", "Javascript"],
-        link: "https://fkcodes.com"
-    },
-    {
-        id: 1,
-        title: "Test",
-        imgUrl: "https://via.placeholder.com/150",
-        stack: ["HTML", "CSS", "Javascript"],
-        link: "https://fkcodes.com"
-    },
-    {
-        id: 2,
-        title: "Test",
-        imgUrl: "https://via.placeholder.com/150",
-        stack: ["HTML", "CSS", "Javascript"],
-        link: "https://fkcodes.com"
+        link: "https://www.lucadelcorona.com/"
     }
 ]
--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,54 +1,34 @@
 export default [
     {
         id: 0,
-        year: 'Today',
-        title: 'Full Stack developer',
-        duration: '6 years',
-        details: 'Design, development and maintenance of hybrid applications for mobile devices and microservices.\n' +
-            'Internally, I performed the following tasks:\n' +
-            '- Design of UI/UX interfaces for mobile applications\n' +
-            '- Develop cross-platform hybrid applications with Flutter and Dart\n' +
-            '- Development of RESTful APIs\n' +
-            '- Design and maintenance of NoSQL databases such as MongoDB\n' +
-            '- Implementation of ads within mobile applications'
+        year: '2022',
+        title: 'Techedge SpA - Junior Consultant',
+        duration: 'Actual',
+        details: 'Sviluppo di nuove funzionalità per applicazioni applicazioni di tipo Enterprise, per clienti business, utilizzando PHP e Symfony, a livello Backend e Frontend.',
+        chips: ["PHP", "Symfony", "REST", "PostgreSQL", "SQL", "Twig", "HTML", "CSS", "Javascript", "AWS"]
     },
     {
         id: 1,
-        year: 'Today',
-        title: 'Full Stack developer',
-        duration: '6 years',
-        details: 'Design, development and maintenance of hybrid applications for mobile devices and microservices.\n' +
-            'Internally, I performed the following tasks:\n' +
-            '- Design of UI/UX interfaces for mobile applications\n' +
-            '- Develop cross-platform hybrid applications with Flutter and Dart\n' +
-            '- Development of RESTful APIs\n' +
-            '- Design and maintenance of NoSQL databases such as MongoDB\n' +
-            '- Implementation of ads within mobile applications'
+        year: '2020',
+        title: 'Infinity Solutions - Full Stack developer',
+        duration: '2 years',
+        details: 'Architettura, sviluppo e manutenzione di applicazioni ibride per dispositivi mobili e web, includendo lo sviluppo di REST API per la comunicazione con database di tipo NoSQL MongoDB. Mansioni che includevano anche la manutenzione e lo sviluppo di nuove funzionalità del database.',
+        chips: ["Dart", "Flutter", "REST", "Javascript", "Typescript", "MongoDB", "HTML", "CSS", "Javascript"]
     },
     {
         id: 2,
-        year: 'Today',
-        title: 'Full Stack developer',
-        duration: '6 years',
-        details: 'Design, development and maintenance of hybrid applications for mobile devices and microservices.\n' +
-            'Internally, I performed the following tasks:\n' +
-            '- Design of UI/UX interfaces for mobile applications\n' +
-            '- Develop cross-platform hybrid applications with Flutter and Dart\n' +
-            '- Development of RESTful APIs\n' +
-            '- Design and maintenance of NoSQL databases such as MongoDB\n' +
-            '- Implementation of ads within mobile applications'
+        year: '2019',
+        title: 'Cisco Networking Academy',
+        duration: '3 years',
+        details: 'Durante gli studi all\'Istituto tecnico "ITIS Galileo Galilei", per la materia "Sistemi e Reti" è stata effettuata una collaborazione tra la scuola e l\'azienda Cisco per proporre i loro corsi durante gli anni di specializzazione.',
+        chips: ["Dart", "Flutter", "REST", "Javascript", "Typescript", "MongoDB", "HTML", "CSS", "Javascript"]
     },
     {
         id: 3,
-        year: 'Today',
-        title: 'Full Stack developer',
-        duration: '6 years',
-        details: 'Design, development and maintenance of hybrid applications for mobile devices and microservices.\n' +
-            'Internally, I performed the following tasks:\n' +
-            '- Design of UI/UX interfaces for mobile applications\n' +
-            '- Develop cross-platform hybrid applications with Flutter and Dart\n' +
-            '- Development of RESTful APIs\n' +
-            '- Design and maintenance of NoSQL databases such as MongoDB\n' +
-            '- Implementation of ads within mobile applications'
+        year: '2017',
+        title: 'ITIS Galileo Galilei',
+        duration: '5 years',
+        details: 'Percorso formativo specializzato in tecnologie informatiche e telecomunicazioni.',
+        chips: ["C", "C++", "Java", "Javascript", "PHP", "SQL", "HTML", "CSS", "MySQL", "MariaDB", "Git", "Github", "Docker", "AWS"]
     }
 ]
