d8412ffb76f17579cc12281e194ee00dbdee2259
luca_delcorona <luca.delcorona@avvale.com>
2025-05-08 18:37:22 +0200

    chore: Updated projects

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,9 +4,9 @@ const Intro = () => {
     return (
         <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
             <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Luca Del Corona</h1>
-            <p className="text-base md:text-xl mb-3 font-medium">Full Stack developer</p>
+            <p className="text-base md:text-xl mb-3 font-medium">Android Native & Cross-Platform Developer</p>
             <p className="text-1xl max-w-xl mb-6">
-                Passionate and curious developer, specialized in the design and implementation of mobile applications and software with various utilities.
+                Developer with solid experience in creating modern digital solutions for the mobile  while  best practices. I take a hands-on, collaborative approach, with a strong focus on innovation and continuous professional growth.
             </p>
         </div>
     );
--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,75 @@
 import portfolioImage from "../assets/portfolio.png";
 import tapItImage from "../assets/tapit.jpg";
+import eSolutionsChargingImage from "../assets/projects/eSolutionsCharging.png";
+import beneAssicurazioniImage from "../assets/projects/beneAssicurazioni.png";
+import eniLiveAppImage from "../assets/projects/eniLiveApp.jpg";
+import rechargeAroundImage from "../assets/projects/rechargeAround.png";
+import yubiqMoreImage from "../assets/projects/yubiqMore.png";
+import sossoldiImage from "../assets/projects/sossoldi.jpg";
+import alphonseClubImage from "../assets/projects/alphonseClub.jpg";
 
 export default [
+    {
+        id: 8,
+        title: "Bene assicurazioni",
+        imgUrl: beneAssicurazioniImage,
+        stack: ["Flutter", "Dart", "Android", "iOS"],
+        link: "https://www.bene.it/"
+    },
+    {
+        id: 7,
+        title: "eSolutions Charging",
+        imgUrl: eSolutionsChargingImage,
+        stack: ["React Native", "Expo", "Android", "iOS", "AWS", "Firebase"],
+        link: "https://www.esolutionscharging.com/it_IT/on-the-go"
+    },
+    {
+        id: 6,
+        title: "Eni Live - Europe",
+        imgUrl: eniLiveAppImage,
+        stack: ["Java", "Android", "AWS"],
+        link: "https://www.enilive.it/app-enilive"
+    },
+    {
+        id: 5,
+        title: "Recharge Around",
+        imgUrl: rechargeAroundImage,
+        stack: ["Java", "Android", "Firebase"],
+        link: "https://www.repower.com/it/e-mobility/recharge-around"
+    },
+    {
+        id: 4,
+        title: "YUBIQ More",
+        imgUrl: yubiqMoreImage,
+        stack: ["Flutter", "Dart", "Android", "iOS", "Firebase"],
+        link: "https://play.google.com/store/apps/details?id=com.docflow.yubik.employee.dossier&hl=it"
+    },
+    {
+        id: 3,
+        title: "Alphonse Club",
+        imgUrl: alphonseClubImage,
+        stack: ["Next.js", "React", "Typescript", "Supabase", "Clerk"],
+        link: "https://www.alphonseclub.com/"
+    },
+    {
+        id: 2,
+        title: "Sossoldi",
+        imgUrl: sossoldiImage,
+        stack: ["Flutter", "Dart", "Android", "iOS"],
+        link: "https://rip-comm.github.io/sossoldi/"
+    },
+    {
+        id: 1,
+        title: "TapIt: Be the fastest finger",
+        imgUrl: tapItImage,
+        stack: ["Flutter", "Dart", "Android", "iOS", "Firebase"],
+        link: "https://github.com/VoidElle/TapIt"
+    },
     {
         id: 0,
         title: "Portfolio",
         imgUrl: portfolioImage,
         stack: ["React", "Javascript", "TailwindCSS"],
         link: "https://github.com/VoidElle/Portfolio"
-    },
-    {
-        id: 1,
-        title: "TapIt: Be the fastest finger",
-        imgUrl: tapItImage,
-        stack: ["Flutter", "Dart", "Android", "iOS"],
-        link: "https://github.com/VoidElle/TapIt"
     },
 ]
