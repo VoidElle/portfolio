b75a0b6aba5733701e16e4938ef95b1df908593a
VoidElle <lucadelc@gmail.com>
2025-12-13 14:46:49 +0100

    chore: Fix vercel analytics + Updated dependecies

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { SpeedInsights } from "@vercel/speed-insights/react";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react";
 
 import App from './App';
 
