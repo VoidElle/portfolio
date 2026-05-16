cfcfacc6e4775fcc3d675fc48bea589122ae0c41
VoidElle <lucadelc@gmail.com>
2026-05-17 00:16:47 +0200

    feat: update portfolio with new projects and remove outdated entries

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -1,61 +1,61 @@
 import portfolioImage from "../assets/portfolio.png";
-import tapItImage from "../assets/tapit.jpg";
 import sossoldiImage from "../assets/projects/sossoldi.jpg";
 import alphonseClubImage from "../assets/projects/alphonseClub.jpg";
-import hassioOpenPico from "../assets/projects/hassioOpenPico.png";
+import openTecnosystemi from "../assets/projects/openTecnosystemi.png";
 import openPicoApp from "../assets/projects/openPicoApp.png";
 import openPicoLocalApi from "../assets/projects/openPicoLocalApi.png";
+import openPolarisLocalApi from "../assets/projects/openPolarisLocalApi.png";
 import blackTheoryImage from "../assets/projects/blackTheory.png";
 
 export default [
     {
         id: 7,
-        title: "Black Theory",
-        imgUrl: blackTheoryImage,
-        stack: ["Flutter", "Dart", "Android", "iOS"],
-        link: "https://github.com/VoidElle/black-theory"
+        title: "Open Tecnosystemi",
+        imgUrl: openTecnosystemi,
+        stack: ["Python", "Home assistant"],
+        link: "https://github.com/VoidElle/hass-open-tecnosystemi"
     },
     {
         id: 6,
-        title: "Open Pico App",
-        imgUrl: openPicoApp,
-        stack: ["Flutter", "Dart", "Android", "iOS"],
-        link: "https://github.com/VoidElle/open-pico-app"
-    },
-    {
-        id: 5,
-        title: "Hassio Open Pico",
-        imgUrl: hassioOpenPico,
-        stack: ["Python", "Home assistant"],
-        link: "https://github.com/VoidElle/hassio-open-pico"
-    },
-    {
-        id: 4,
         title: "Open Pico Local API",
         imgUrl: openPicoLocalApi,
         stack: ["Python", "UDP"],
         link: "https://github.com/VoidElle/open-pico-local-api"
     },
     {
+        id: 5,
+        title: "Open Polaris Local API",
+        imgUrl: openPolarisLocalApi,
+        stack: ["Python", "TCP"],
+        link: "https://github.com/VoidElle/open-polaris-local-api"
+    },
+    {
+        id: 4,
+        title: "Black Theory",
+        imgUrl: blackTheoryImage,
+        stack: ["Flutter", "Dart", "Android", "iOS"],
+        link: "https://github.com/VoidElle/black-theory"
+    },
+    {
         id: 3,
+        title: "Open Pico App",
+        imgUrl: openPicoApp,
+        stack: ["Flutter", "Dart", "Android", "iOS"],
+        link: "https://github.com/VoidElle/open-pico-app"
+    },
+    {
+        id: 2,
         title: "Alphonse Club",
         imgUrl: alphonseClubImage,
         stack: ["Next.js", "React", "Typescript", "Tailwind CSS", "Supabase", "Clerk"],
         link: "https://www.alphonseclub.com/"
     },
     {
-        id: 2,
+        id: 1,
         title: "Sossoldi",
         imgUrl: sossoldiImage,
         stack: ["Flutter", "Dart", "Android", "iOS"],
         link: "https://rip-comm.github.io/sossoldi/"
-    },
-    {
-        id: 1,
-        title: "TapIt: Be the fastest finger",
-        imgUrl: tapItImage,
-        stack: ["Flutter", "Dart", "Android", "iOS", "Firebase"],
-        link: "https://github.com/VoidElle/TapIt"
     },
     {
         id: 0,
